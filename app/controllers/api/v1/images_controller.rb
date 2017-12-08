dba1c130d2c1b89ead3356c2f426383cc0ab8bce
natgit8 <natasha.gitlin8@gmail.com>
2017-12-07 23:52:01 -0500

    image controller

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -9,19 +9,19 @@ class Api::V1::ImagesController < ApplicationController
     render json: images
   end
 
-  def scrape
-    response = HTTParty.get('https://nytimes.com')
-    parse_page = Nokogiri::HTML(response)
-    i = 1
-    imgs =[]
-    while i < 71
-      imgs << parse_page.css("body > main > div > article > section > div.sources-container.row > div:nth-child(#{i}) > a > img")
-      i += 1
-    end
-    imgs.flatten.map do |img|
-      Image.create(name: img.attributes['alt'].value, image_url: img.attributes['image_url'].value)
-    end
-  end
+  # def scrape
+  #   response = HTTParty.get('https://nytimes.com')
+  #   parse_page = Nokogiri::HTML(response)
+  #   i = 1
+  #   imgs =[]
+  #   while i < 71
+  #     imgs << parse_page.css("body > main > div > article > section > div.sources-container.row > div:nth-child(#{i}) > a > img")
+  #     i += 1
+  #   end
+  #   imgs.flatten.map do |img|
+  #     Image.create(name: img.attributes['alt'].value, image_url: img.attributes['image_url'].value)
+  #   end
+  # end
 
   def fetch_sources_from_api
     response = HTTParty.get("https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=#{ENV['NEWS_API_KEY']}")
