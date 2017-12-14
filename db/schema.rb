5c5b2da68c92c50b59d7a761a79b53832b37f740
natgit8 <natasha.gitlin8@gmail.com>
2017-12-14 00:05:25 -0500

    drop img table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207205918) do
+ActiveRecord::Schema.define(version: 20171118223412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,13 +24,6 @@ ActiveRecord::Schema.define(version: 20171207205918) do
     t.datetime "updated_at", null: false
     t.text     "abstract"
     t.string   "image_url"
-  end
-
-  create_table "images", force: :cascade do |t|
-    t.string   "name"
-    t.string   "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
