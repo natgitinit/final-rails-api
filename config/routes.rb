ccae6a6f8d6c5001b5805944489a0836aec9091a
natgitinit <natgitinit@gmail.com>
2023-08-01 12:14:25 -0500

    routes and whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
       post 'auth_user' => 'authentication#authenticate_user'
     end
   end
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
